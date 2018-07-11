b2fb74f83a9d60c352342144833337f6d2c71023
Kael Shipman <kael.shipman@gmail.com>
2018-07-10 19:08:52 -0500

    Restructured repo around new build/publish system

---
+++ scripts/build.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+set -e
+
+function setup_env() {
+    setup_deb_env $@
+    rm -Rf "$builddir"/*
+}
+
+function place_files() {
+    local pkgname="$1"
+    local targdir="$2"
+    local pkgtype="$3"
+
+    if [ "$pkgname" == "timetraveler" ]; then
+        cp -R src/* "$targdir/"
+    else
+        >&2 echo
+        >&2 echo "E: Don't know how to handle packages of type $pkgtype"
+        >&2 echo
+        exit 14
+    fi
+}
+
+function build_package() {
+    pkgtype="$1"
+    shift
+
+    if [ "$pkgtype" == "deb" ]; then
+        build_deb_package $@
+    else
+        >&2 echo
+        >&2 echo "E: Don't know how to build packages of type '$pkgtype'"
+        >&2 echo
+        exit 11
+    fi
+}
+
+# Include the library and go
+if [ -z "$KSSTDLIBS_PATH" ]; then 
+    KSSTDLIBS_PATH=/usr/lib/ks-std-libs
+fi
+if [ ! -e "$KSSTDLIBS_PATH/libpkgbuilder.sh" ]; then
+    >&2 echo
+    >&2 echo "E: Your system doesn't appear to have ks-std-libs installed. (Looking for"
+    >&2 echo "   library 'libpkgbuilder.sh' in $KSSTDLIBS_PATH. To define a different"
+    >&2 echo "   place to look for this file, just export the 'KSSTDLIBS_PATH' environment"
+    >&2 echo "   variable.)"
+    >&2 echo
+    exit 4
+else
+    . "$KSSTDLIBS_PATH/libpkgbuilder.sh"
+    build
+fi
+
--- tests/libtest.sh
+++ tests/libtest.sh
@@ -1,199 +1,34 @@
 #!/bin/bash
 
-inc=timetraveler.lib.sh
+inc=libtimetraveler.sh
 if [ ! -e "$inc" ]; then
-    inc="src/$inc"
+    inc="timetraveler/$inc"
     if [ ! -e "$inc" ]; then
-        inc="../$inc"
+        inc="lib/$inc"
         if [ ! -e "$inc" ]; then
-            >&2 echo
-            >&2 echo "E: Can't find library. Please run this command from repo root"
-            >&2 echo
-            exit 1
+            inc="usr/$inc"
+            if [ ! -e "$inc" ]; then
+                inc="src/$inc"
+                if [ ! -e "$inc" ]; then
+                    inc="../$inc"
+                    if [ ! -e "$inc" ]; then
+                        >&2 echo
+                        >&2 echo "E: Can't find library. Please run this command from repo root"
+                        >&2 echo
+                        exit 1
+                    fi
+                fi
+            fi
         fi
     fi
 fi
 
 . "$inc"
 
-# Set config variables, then override if possible
-
-TT_TEST_REMOTE_HOST=kaelshipman.me
-TT_TEST_REMOTE_PATH_IN_HOME=.profile
-
-if [ -e ~/.config/timetraveler/test-config.sh ]; then
-    . ~/.config/timetraveler/test-config.sh
-fi
-
-
-test_path_is_remote_handles_normal_paths() {
-    local path
-
-    # local path
-    path="/some/crazy:delim/localpath"
-    assert_status_code 1 'path_is_remote "'$path'"' "Local path '$path' should be identified as local"
-
-    # remote path
-    path="kael@kaelshipman.me:/some/path"
-    assert_status_code 0 'path_is_remote "'$path'"' "Remote path '$path' should be identified as remote"
-}
-
-test_path_is_remote_handles_paths_with_spaces() {
-    local path
-    local cmd
-
-    # local path
-    path="/some/crazy:delim/localpath/with spaces"
-    cmd="path_is_remote '"$path"'"
-    assert_status_code 1 'path_is_remote "'$path'"' "Local path '$path' should be identified as local"
-
-    # remote path
-    path="kael@kaelshipman.me:/some/path with spaces"
-    cmd="path_is_remote '"$path"'"
-    assert_status_code 0 "$cmd" "Remote path '$path' should be identified as remote"
-}
-
-todo_test_path_is_remote_handles_paths_with_special_chars() {
-    local path
-    local cmd
-
-    # local
-    path="/some/path with spaces/& some/'special!' "'"'"chars"'"'"/path"
-    cmd="path_is_remote '"$(echo "$path" | sed "s/'/'"'"'"'"'"'"'/g")"'"
-    assert_status_code 1 "$cmd" "Local path '$path' should be identified as local"
-
-    # remote
-    path="kael@kaelshipman.me:/some/path with spaces/& some/'special!' "'"'"chars"'"'"/path"
-    cmd="path_is_remote '"$(echo "$path" | sed "s/'/'"'"'"'"'"'"'/g")"'"
-    assert_status_code 0 "$cmd" "Remote path '$path' should be identified as remote"
-}
-
-test_path_is_remote_complains_with_bad_arguments() {
-    assert_status_code 26 "path_is_remote '/this is a/path/with/spaces' 'but this is just a string'" "Should have complained for multiple arguments"
-    assert_status_code 26 "path_is_remote" "Should have complained with no arguments"
-}
-
-test_path_exists_with_valid_paths() {
-    assert_status_code 0 "path_exists '/tmp'"
-    assert_status_code 0 "path_exists '$TT_TEST_REMOTE_HOST:/tmp'"
-    assert_status_code 0 "path_exists '$TT_TEST_REMOTE_HOST:$TT_TEST_REMOTE_PATH_IN_HOME'"
-}
-
-test_path_exists_with_nonexistent_paths() {
-    assert_status_code 1 "path_exists '/bunk'"
-    assert_status_code 1 "path_exists '$TT_TEST_REMOTE_HOST:/bunk'"
-    assert_status_code 1 "path_exists '$TT_TEST_REMOTE_HOST:bunk'"
-}
-
-test_path_exists_with_paths_with_spaces() {
-    local path="/tmp/path with spaces/here"
-
-    mkdir -p "$path"
-    assert_status_code 0 "path_exists '$path'"
-    rm -Rf "$path"
-
-    ssh "$TT_TEST_REMOTE_HOST" 'mkdir -p "'$path'"'
-    assert_status_code 0 "path_exists '$TT_TEST_REMOTE_HOST:$path'"
-    ssh "$TT_TEST_REMOTE_HOST" 'rm -Rf "'$path'"'
-}
-
-test_path_exists_with_bad_arguments() {
-    assert_status_code 26 "path_exists '/this is a/path/with/spaces' 'but this is just a string'" "Should have complained for multiple arguments"
-    assert_status_code 26 "path_exists" "Should have complained with no arguments"
-}
-
-todo_test_path_exists_with_special_chars() {
-    local path="/some/path with spaces/& some/'special!' "'"'"chars"'"'"/path"
-    local cmd
-
-    # local
-    mkdir -p "$path"
-    cmd="path_exists '"$(echo "$path" | sed "s/'/'"'"'"'"'"'"'/g")"'"
-    assert_status_code 0 "$cmd"
-
-    # remote
-    ssh "$TT_TEST_REMOTE_HOST" 'mkdir -p "'$path'"'
-    assert_status_code 0 "path_exists '$TT_TEST_REMOTE_HOST:$(echo "$path" | sed "s/'/'"'"'"'"'"'"'/g")'"
-    ssh "$TT_TEST_REMOTE_HOST" 'rm -Rf "'$path'"'
-}
-
-test_tt_cmd_can_make_normal_dirs() {
-    local path="/tmp/some/long/path"
-
-    tt_cmd "$path" 'mkdir -p "::path::"'
-    assert "test -e '$path'"
-    rm -Rf /tmp/some
-
-    local remoteAddr="$TT_TEST_REMOTE_HOST:$path"
-    tt_cmd "$remoteAddr" 'mkdir -p "::path::"'
-    assert "ssh '$TT_TEST_REMOTE_HOST' 'test -e "'"'"$path"'"'"'"
-    ssh "$TT_TEST_REMOTE_HOST" "rm -Rf '/tmp/some'"
-}
-
-test_tt_cmd_can_make_dirs_with_spaces() {
-    local path="/tmp/some/long path with/some spaces"
-
-    tt_cmd "$path" 'mkdir -p "::path::"'
-    assert "test -e '$path'"
-    rm -Rf /tmp/some
-
-    local remoteAddr="$TT_TEST_REMOTE_HOST:$path"
-    tt_cmd "$remoteAddr" 'mkdir -p "::path::"'
-    assert "ssh '$TT_TEST_REMOTE_HOST' 'test -e "'"'"$path"'"'"'"
-    ssh "$TT_TEST_REMOTE_HOST" "rm -Rf '/tmp/some'"
-}
-
-todo_test_tt_cmd_can_make_dirs_with_special_chars() {
+function todo_test_verify_config() {
     assert_fail
 }
 
-test_tt_cmd_can_move_normal_dirs() {
-    local toPath="/tmp/test"
-    local fromPath="$toPath.partial"
-
-    # just in case...
-    tt_cmd "$toPath" 'rm -Rf "::path::"'
-
-    tt_cmd "$fromPath" 'mkdir -p "::path::"'
-    tt_cmd "$fromPath" 'mv "::path::" "'"$toPath"'"'
-    assert 'path_exists "'"$toPath"'"'
-
-    tt_cmd "$toPath" 'rm -Rf "::path::"'
-
-    # just in case...
-    tt_cmd "$TT_TEST_REMOTE_HOST:$toPath" 'rm -Rf "::path::"'
-
-    tt_cmd "$TT_TEST_REMOTE_HOST:$fromPath" 'mkdir -p "::path::"'
-    tt_cmd "$TT_TEST_REMOTE_HOST:$fromPath" 'mv "::path::" "'"$toPath"'"'
-    assert 'path_exists "'"$TT_TEST_REMOTE_HOST:$toPath"'"'
-
-    tt_cmd "$TT_TEST_REMOTE_HOST:$toPath" 'rm -Rf "::path::"'
-}
-
-test_tt_cmd_can_move_dirs_with_spaces() {
-    local toPath="/tmp/test dir with/spaces"
-    local fromPath="$toPath.partial"
-
-    # just in case...
-    tt_cmd "$toPath" 'rm -Rf "::path::"'
-
-    tt_cmd "$fromPath" 'mkdir -p "::path::"'
-    tt_cmd "$fromPath" 'mv "::path::" "'"$toPath"'"'
-    assert 'path_exists "'"$toPath"'"'
-
-    tt_cmd "$toPath" 'rm -Rf "::path::"'
-
-    # just in case...
-    tt_cmd "$TT_TEST_REMOTE_HOST:$toPath" 'rm -Rf "::path::"'
-
-    tt_cmd "$TT_TEST_REMOTE_HOST:$fromPath" 'mkdir -p "::path::"'
-    tt_cmd "$TT_TEST_REMOTE_HOST:$fromPath" 'mv "::path::" "'"$toPath"'"'
-    assert 'path_exists "'"$TT_TEST_REMOTE_HOST:$toPath"'"'
-
-    tt_cmd "$TT_TEST_REMOTE_HOST:$toPath" 'rm -Rf "::path::"'
-}
-
-todo_test_tt_cmd_can_move_dirs_with_special_chars() {
+function todo_test_parse_config() {
     assert_fail
 }
-
